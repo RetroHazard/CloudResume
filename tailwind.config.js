d9b32e3cda33993bcccffe47ea81bb8d2d73dcde
RetroHazard <Griever92@gmail.com>
2024-04-25 16:02:03 +0900

    Style Updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{html,js}'],
+  plugins: [
+    require('tailwindcss-animated')
+  ],
   theme: {
     colors: {
       content: {
