b62739d4a0c6c821a21c27746ee5b27c1f0f346d
RetroHazard <Griever92@gmail.com>
2024-05-09 11:38:48 +0900

    Add Visitor Counter

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import { faHouse, faGraduationCap, faBriefcase, faChartSimple, faClipboardCheck, faLayerGroup, faMessage } from "@fortawesome/free-solid-svg-icons";
+import { faHouse, faGraduationCap, faBriefcase, faChartSimple, faClipboardCheck, faLayerGroup, faMessage, faUsers } from "@fortawesome/free-solid-svg-icons";
 import { NavLink } from "react-router-dom";
 
+const NUM_VISITS = 0; //TODO: Record and Get Value from DynamoDB
 
 function Navigation() {
     return (
@@ -11,7 +12,7 @@ function Navigation() {
             <nav className="flex float-left bg-secondary-700 p-2 rounded-lg mr-5 mt-28 h-fit" id="navbar">
                 <ul className="flex-col text-sm font-medium text-content-accent">
                     <li>
-                        <NavLink to={"/"} className={({ isActive }) => {
+                        <NavLink to={"/"} className={({isActive}) => {
                             return isActive ? "nav-block-active" : "nav-block-inactive";
                         }}
                         >
@@ -22,7 +23,7 @@ function Navigation() {
                         </NavLink>
                     </li>
                     <li>
-                        <NavLink to={"/education"} className={({ isActive }) => {
+                        <NavLink to={"/education"} className={({isActive}) => {
                             return isActive ? "nav-block-active" : "nav-block-inactive";
                         }}
                         >
@@ -33,7 +34,7 @@ function Navigation() {
                         </NavLink>
                     </li>
                     <li>
-                        <NavLink to={"/experience"} className={({ isActive }) => {
+                        <NavLink to={"/experience"} className={({isActive}) => {
                             return isActive ? "nav-block-active" : "nav-block-inactive";
                         }}
                         >
@@ -44,7 +45,7 @@ function Navigation() {
                         </NavLink>
                     </li>
                     <li>
-                        <NavLink to={"/skills"} className={({ isActive }) => {
+                        <NavLink to={"/skills"} className={({isActive}) => {
                             return isActive ? "nav-block-active" : "nav-block-inactive";
                         }}
                         >
@@ -55,7 +56,7 @@ function Navigation() {
                         </NavLink>
                     </li>
                     <li>
-                        <NavLink to={"/certifications"} className={({ isActive }) => {
+                        <NavLink to={"/certifications"} className={({isActive}) => {
                             return isActive ? "nav-block-active" : "nav-block-inactive";
                         }}
                         >
@@ -66,7 +67,7 @@ function Navigation() {
                         </NavLink>
                     </li>
                     <li>
-                        <NavLink to={"/projects"} className={({ isActive }) => {
+                        <NavLink to={"/projects"} className={({isActive}) => {
                             return isActive ? "nav-block-active" : "nav-block-inactive";
                         }}
                         >
@@ -77,7 +78,7 @@ function Navigation() {
                         </NavLink>
                     </li>
                     <li>
-                        <NavLink to={"/contact"} className={({ isActive }) => {
+                        <NavLink to={"/contact"} className={({isActive}) => {
                             return isActive ? "nav-block-active" : "nav-block-inactive";
                         }}
                         >
@@ -87,8 +88,19 @@ function Navigation() {
                             Contact
                         </NavLink>
                     </li>
+                    <div className="justify-between bg-secondary-600 h-0.5 mt-2 mb-2 w-full"></div>
+                    <li className="flex-col text-sm font-medium text-content-accent">
+                        <div
+                            className="inline-flex items-center max-h-8 px-4 py-3 mt-1 mb-1.5 rounded-lg bg-secondary-800 w-full">
+                            <div className="icon-box">
+                                <i><FontAwesomeIcon icon={faUsers}/></i>
+                            </div>
+                            {NUM_VISITS} Visitors
+                        </div>
+                    </li>
                 </ul>
             </nav>
+
         </>
     )
 }
