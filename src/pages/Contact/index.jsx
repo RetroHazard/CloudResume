a55c2a1609756762ec1050eda893203385042e18
RetroHazard <Griever92@gmail.com>
2024-05-13 12:18:22 +0900

    Component Update
    Remove unused packages
    Install Formik
    Refactor existing Form

--- src/pages/Contact/index.jsx
+++ src/pages/Contact/index.jsx
@@ -1,7 +1,88 @@
 import React from "react";
+import { Formik, Form, useField, Field, ErrorMessage } from "formik";
 
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faPaperPlane} from "@fortawesome/free-solid-svg-icons";
+
+
+const initialValues = {
+    firstName: '',
+    lastName: '',
+    email: '',
+    subject: '',
+    message: '',
+};
+
+const onSubmit = (values) => {
+    console.log(values);
+};
+
+const validate = (values) => {
+    const errors = {};
+
+    if (!values.firstName & !values.lastName) {
+        errors.fullName = 'Required';
+    }
+
+    if (!values.email) {
+        errors.email = 'Required';
+    } else if (!/^\S+@\S+\.\S+$/.test(values.email)) {
+        errors.email = 'Invalid Email Address';
+    }
+
+    if (!values.subject) {
+        errors.subject = 'Required';
+    }
+
+    if (!values.message) {
+        errors.message = 'Required'
+    }
+
+    return errors;
+};
+
+const ContactForm = () => {
+    return (
+        <div>
+            <Formik initialValues={initialValues} onSubmit={onSubmit} validate={validate}>
+                {({ isSbumitting }) => (
+        <form className="space-y-8">
+            <div className="flex flex-col">
+                <label htmlFor="fullName" className="block mb-2 text-sm font-medium text-content-subtitle">Name</label>
+                <div className="flex flex-row gap-6">
+                    <input type="text" id="firstName" className="text-entry" name="firstName" placeholder="First Name"/>
+                    <input type="text" id="lastName" className="text-entry" name="lastName" placeholder="Last Name"/>
+                    <ErrorMessage name="fullName" />
+                </div>
+            </div>
+            <div>
+                <label htmlFor="email" className="block mb-2 text-sm font-medium text-content-subtitle">Email</label>
+                <input type="email" id="email" className="text-entry" name="email" placeholder="name@email.com"/>
+                <ErrorMessage name="email" />
+            </div>
+            <div>
+                <label htmlFor="subject" className="block mb-2 text-sm font-medium text-content-subtitle">Subject</label>
+                <input type="text" id="subject" className="text-entry" name="subject" placeholder="What's up?"/>
+            </div>
+            <div className="sm:col-span-2">
+                <label htmlFor="message" className="block mb-2 text-sm font-medium text-content-subtitle">Your Message</label>
+                <textarea id="message" rows="4" className="text-entry" name="message" placeholder="Leave a comment..."></textarea>
+            </div>
+            <button type="submit" disabled={isSbumitting} className="bg-primary-500 hover:bg-primary-400 text-secondary-800 font-bold py-2 px-4 rounded inline-flex items-center">
+                <div className="icon-box">
+                    <i className="w-5 h-5 mr-2 text-content-icons">
+                        <FontAwesomeIcon icon={faPaperPlane}/>
+                    </i>
+                </div>
+                <span className="text-content-header text-sm">Send Message</span>
+            </button>
+        </form>
+        )}
+        </Formik>
+        </div>
+    );
+};
+
 
 function Contact() {
     return (
@@ -13,35 +94,7 @@ function Contact() {
                         Have a potential opportunity or project that you'd like to collaborate on?
                         <br/>
                         Feel free to reach out using the contact form below.</p>
-                    <form action="#" className="space-y-8">
-                        <div className="flex flex-col">
-                            <label htmlFor="name" className="block mb-2 text-sm font-medium text-content-subtitle">Name</label>
-                            <div className="flex flex-row gap-6">
-                                <input type="text" id="firstname" className="text-entry" placeholder="First Name" required/>
-                                <input type="text" id="lastname" className="text-entry" placeholder="Last Name" required/>
-                            </div>
-                        </div>
-                        <div>
-                            <label htmlFor="email" className="block mb-2 text-sm font-medium text-content-subtitle">Email</label>
-                            <input type="email" id="email" className="text-entry" placeholder="name@email.com" required/>
-                        </div>
-                        <div>
-                            <label htmlFor="subject" className="block mb-2 text-sm font-medium text-content-subtitle">Subject</label>
-                            <input type="text" id="subject" className="text-entry" placeholder="What's up?" required/>
-                        </div>
-                        <div className="sm:col-span-2">
-                            <label htmlFor="message" className="block mb-2 text-sm font-medium text-content-subtitle">Your Message</label>
-                            <textarea id="message" rows="4" className="text-entry" placeholder="Leave a comment..."></textarea>
-                        </div>
-                        <button type="submit" className="bg-primary-500 hover:bg-primary-400 text-secondary-800 font-bold py-2 px-4 rounded inline-flex items-center">
-                            <div className="icon-box">
-                                <i className="w-5 h-5 mr-2 text-content-icons">
-                                    <FontAwesomeIcon icon={faPaperPlane}/>
-                                </i>
-                            </div>
-                            <span className="text-content-header text-sm">Send Message</span>
-                        </button>
-                    </form>
+                    <ContactForm />
                 </div>
             </div>
         </>
