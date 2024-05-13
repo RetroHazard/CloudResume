1d47e260f6c2baf06907f5ab2e579c6ba8729be0
RetroHazard <Griever92@gmail.com>
2024-05-13 21:56:47 +0900

    Update ContactForm
    Build new Contact Form to utilize Lambda and Amazon SES

--- src/pages/Contact/index.jsx
+++ src/pages/Contact/index.jsx
@@ -1,7 +1,69 @@
 import React from "react";
+import { useState } from "react";
 
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faPaperPlane} from "@fortawesome/free-solid-svg-icons";
+
+const ContactForm = () => {
+    const [firstName, setFirstName] = useState('')
+    const [lastName, setLastName] = useState('')
+    const [email, setEmail] = useState('')
+    const [subject, setSubject] = useState('')
+    const [message, setMessage] = useState('')
+
+    const endpoint = '' //TODO: Add API Endpoint/Lambda call
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        const data = { firstName, lastName, email, subject, message }
+
+        const fetchPromise = fetch(endpoint, {
+            method: 'POST',
+            mode: 'cors',
+            cache: 'no-cache',
+            body: JSON.stringify(data)
+          });
+          fetchPromise
+            .then(response => response.json())
+            .then(data => {
+              console.log(data); // handle response, catch errors
+          })
+    }
+
+    return (
+        <form action={endpoint} onSubmit={handleSubmit} method="POST" className="space-y-8">
+            <div className="flex flex-col">
+                <label htmlFor="name" className="block mb-2 text-sm font-medium text-content-subtitle">Name</label>
+                <div className="flex flex-row gap-6">
+                    <input type="text" id="firstName" className="text-entry" placeholder="First Name"  value={firstName} onChange={(e) => setFirstName(e.target.value)} required/>
+                    <input type="text" id="lastName" className="text-entry" placeholder="Last Name"  value={lastName} onChange={(e) => setLastName(e.target.value)} required/>
+                </div>
+            </div>
+            <div>
+                <label htmlFor="email" className="block mb-2 text-sm font-medium text-content-subtitle">Email</label>
+                <input type="email" id="email" className="text-entry" placeholder="name@email.com" value={email} onChange={(e) => setEmail(e.target.value)} required/>
+            </div>
+            <div>
+                <label htmlFor="subject" className="block mb-2 text-sm font-medium text-content-subtitle">Subject</label>
+                <input type="text" id="subject" className="text-entry" placeholder="What's up?" value={subject} onChange={(e) => setSubject(e.target.value)} required/>
+            </div>
+            <div className="sm:col-span-2">
+                <label htmlFor="message" className="block mb-2 text-sm font-medium text-content-subtitle">Your Message</label>
+                <textarea id="message" rows="5" className="text-entry" placeholder="Leave a comment..." value={message} onChange={(e) => setMessage(e.target.value)}></textarea>
+            </div>
+            <button type="submit" className="bg-primary-500 hover:bg-primary-400 text-secondary-800 font-bold py-2 px-4 rounded inline-flex items-center">
+                <div className="icon-box">
+                    <i className="w-5 h-5 mr-2 text-content-icons">
+                        <FontAwesomeIcon icon={faPaperPlane}/>
+                    </i>
+                </div>
+                <span className="text-content-header text-sm">Send Message</span>
+            </button>
+        </form>
+    )
+
+}
+
 
 function Contact() {
     return (
@@ -13,35 +75,7 @@ function Contact() {
                         Have a potential opportunity or project that you'd like to collaborate on?
                         <br/>
                         Feel free to reach out using the contact form below.</p>
-                    <form action="#" className="space-y-8">
-                        <div className="flex flex-col">
-                            <label htmlFor="name" className="block mb-2 text-sm font-medium text-content-subtitle">Name</label>
-                            <div className="flex flex-row gap-6">
-                                <input type="text" id="firstname" className="text-entry" placeholder="First Name" required/>
-                                <input type="text" id="lastname" className="text-entry" placeholder="Last Name" required/>
-                            </div>
-                        </div>
-                        <div>
-                            <label htmlFor="email" className="block mb-2 text-sm font-medium text-content-subtitle">Email</label>
-                            <input type="email" id="email" className="text-entry" placeholder="name@email.com" required/>
-                        </div>
-                        <div>
-                            <label htmlFor="subject" className="block mb-2 text-sm font-medium text-content-subtitle">Subject</label>
-                            <input type="text" id="subject" className="text-entry" placeholder="What's up?" required/>
-                        </div>
-                        <div className="sm:col-span-2">
-                            <label htmlFor="message" className="block mb-2 text-sm font-medium text-content-subtitle">Your Message</label>
-                            <textarea id="message" rows="4" className="text-entry" placeholder="Leave a comment..."></textarea>
-                        </div>
-                        <button type="submit" className="bg-primary-500 hover:bg-primary-400 text-secondary-800 font-bold py-2 px-4 rounded inline-flex items-center">
-                            <div className="icon-box">
-                                <i className="w-5 h-5 mr-2 text-content-icons">
-                                    <FontAwesomeIcon icon={faPaperPlane}/>
-                                </i>
-                            </div>
-                            <span className="text-content-header text-sm">Send Message</span>
-                        </button>
-                    </form>
+                    <ContactForm />
                 </div>
             </div>
         </>
