2e996e87a358c0d4371bbfd498663e1aa167c557
RetroHazard <Griever92@gmail.com>
2024-05-16 19:23:31 +0900

    FontAwesome Removed from Packages Installed Iconify-Icon Iconify deployed on Home Page Tested Icon Deployment via JSON data

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,8 @@
 import React from "react";
+import { Icon } from '@iconify-icon/react';
 
 import personalData from '../../assets/json/personal_data.json';
 import socialData from '../../assets/json/social_data.json';
-
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faCloudDownload, faYenSign,} from "@fortawesome/free-solid-svg-icons";
-import { faLinkedinIn, faGithub, faArtstation, faInstagram } from "@fortawesome/free-brands-svg-icons";
 
 function Home() {
     return (
@@ -13,11 +10,13 @@ function Home() {
             <div className="content-block" id="profile">
                 <div className="flex flex-col gap-6 items-start sm:flex-row">
                     <div className="flex items-center gap-4 sm:flex-col">
-                        <img className="rounded-lg h-24 max-w-none md:h-52 md:w-52 sm:h-36 sm:w-36 w-24" src={personalData.profilePicture} alt="" />
+                        <img className="rounded-lg h-24 max-w-none md:h-52 md:w-52 sm:h-36 sm:w-36 w-24" src={personalData.profilePicture}/>
                         <a href="." className="font-bold"> {/* TODO: Retrieve CV File from S3 */}
                             <button className="bg-primary-500 hover:bg-primary-400 text-secondary-800 font-bold py-2 px-4 rounded inline-flex items-center">
                                 <div className="icon-box">
-                                    <i className="w-5 h-5 mr-2 text-content-icons"><FontAwesomeIcon icon={faCloudDownload} size="lg" /></i>
+                                    <i className="w-5 h-5 mr-2 text-content-icons">
+                                        <Icon className="icon-box" icon="ic:outline-cloud-download" width={25} height={25}/>
+                                    </i>
                                 </div>
                                 <span className="text-content-header text-sm">Download CV</span>
                             </button>
@@ -30,18 +29,26 @@ function Home() {
                                 <h2 className="sm:text-lg text-base font-medium text-content-header mb-0">{personalData.jobTitle}</h2>
                             </div>
                             {/* Begin Social Links Section */}
-                            <div className="flex gap-3 flex-wrap sm:flex-nowrap"> {/* TODO: Write Component Function to Automatically Build Social Buttons */}
+                            <div className="flex gap-3 flex-wrap sm:flex-nowrap">  {/*TODO: Write Component Function to Automatically Build Social Buttons*/}
                                 <a className="social-link" href={socialData.LinkedIn} aria-label="LinkedIn">
-                                    <i className="text-base text-content-icons"><FontAwesomeIcon icon={faLinkedinIn} size={"lg"}/></i>
+                                    <i className="text-base text-content-icons">
+                                        <Icon className="social-link" icon="akar-icons:linkedin-v2-fill"/>
+                                    </i>
                                 </a>
                                 <a className="social-link" href={socialData.GitHub} aria-label="Github">
-                                    <i className="text-base text-content-icons"><FontAwesomeIcon icon={faGithub} size={"lg"}/></i>
+                                    <i className="text-base text-content-icons">
+                                        <Icon className="social-link" icon="akar-icons:github-fill"/>
+                                    </i>
                                 </a>
                                 <a className="social-link" href={socialData.ArtStation} aria-label="ArtStation">
-                                    <i className="text-base text-content-icons"><FontAwesomeIcon icon={faArtstation} size={"lg"}/></i>
+                                    <i className="text-base text-content-icons">
+                                        <Icon className="social-link" icon="mdi:artstation"/>
+                                    </i>
                                 </a>
                                 <a className="social-link" href={socialData.Instagram} aria-label="Instagram">
-                                    <i className="text-base text-content-icons"><FontAwesomeIcon icon={faInstagram} size={"lg"}/></i>
+                                    <i className="text-base text-content-icons">
+                                        <Icon className="social-link" icon="akar-icons:instagram-fill"/>
+                                    </i>
                                 </a>
                             </div>
                             {/* End Social Links Section */}
@@ -58,7 +65,7 @@ function Home() {
                                 <div>
                                     <span className="font-medium text-content-subtitle">Salary: </span>
                                     <span className="text-content-accent">
-                                        <FontAwesomeIcon icon={faYenSign} className="mr-1"/>
+                                        <Icon className="icon-box pb-1" icon={personalData.currency_icon}/>
                                         {personalData.salary} {personalData.currency}
                                     </span>
                                 </div>
