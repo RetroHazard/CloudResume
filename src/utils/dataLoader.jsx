3cf78b0ba4c4adc29d1b535fa12eae2aee5006ae
RetroHazard <Griever92@gmail.com>
2024-05-21 14:27:54 +0900

    Renamed prop url -> file as target is generally a json file

--- src/utils/dataLoader.jsx
+++ src/utils/dataLoader.jsx
@@ -1,20 +1,20 @@
 import React, { useState, useEffect } from 'react';
 import {jsonLoader} from "./jsonLoader";
 
-const DataLoader = ({ url, children }) => {
+const DataLoader = ({ file, children }) => {
     const [data, setData] = useState(null);
 
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const loadedData = await jsonLoader(url);
+                const loadedData = await jsonLoader(file);
                 setData(loadedData);
             } catch (error) {
                 console.error('Failed to load data:', error);
             }
         };
         fetchData();
-    }, [url]);
+    }, [file]);
 
     if (!data) {
         return <div>Loading...</div>;
