0398462a73daccd8dc6b3857ed83e68f5f854902
RetroHazard <Griever92@gmail.com>
2024-05-21 10:46:54 +0900

    Fix Key/Index Error

--- src/components/skill_highlight.jsx
+++ src/components/skill_highlight.jsx
@@ -9,9 +9,9 @@ const SkillHighlight =() => {
     return (
         <div className="flex flex-wrap gap-x-8 gap-y-4">
             {skillData.map((skill, index) => (
-                <div className="flex flex-col gap-2">
+                <div key={index} className="flex flex-col gap-2">
                     <div className="flex items-center justify-between">
-                        <a key={index} className="flex gap-2.5 items-center no-underline" href={skill.website} target="_blank" rel="noopener noreferrer">
+                        <a className="flex gap-2.5 items-center no-underline" href={skill.website} target="_blank" rel="noopener noreferrer">
                             <Icon inline icon={skill.logo} width="1.6em" height="1.6em"/>
                             <div className="flex flex-col">
                                 <span className="font-semibold text-base text-content-subtitle hover:text-secondary-100">{skill.name}</span>
@@ -21,7 +21,7 @@ const SkillHighlight =() => {
                     </div>
                     <div className="flex gap-1">
                         <div className="skill-progress-bar-outline">
-                            <div key={index} className="skill-progress-bar" style={{width: skill.level}}/>
+                            <div className="skill-progress-bar" style={{width: skill.level}}/>
                         </div>
                     </div>
                 </div>
