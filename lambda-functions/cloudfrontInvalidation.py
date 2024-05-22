b39b45da161e64331aa417723e3d741dd2e4e3f9
RetroHazard <Griever92@gmail.com>
2024-05-22 13:45:08 +0900

    Update CloudFront Invalidation function for Lambda

--- lambda-functions/cloudfrontInvalidation.py
+++ lambda-functions/cloudfrontInvalidation.py
@@ -4,22 +4,15 @@ import boto3
 import time
 
 def lambda_handler(event, context):
-    path = "/" + event["Records"][0]["s3"]["object"]["key"]
-    bucket_name = event["Records"][0]["s3"]["bucket"]["name"]
-
-    client = boto3.client('s3')
-    tags = client.get_bucket_tagging(Bucket=bucket_name)
-    for tag in tags["TagSet"]:
-        if tag["Key"] == "distribution_id":
-            distribution_id = tag["Value"]
-            break
-
+    for items in event["Records"]:
+        path = "/" + items["s3"]["object"]["key"]
+    print(path)
     client = boto3.client('cloudfront')
-    invalidation = client.create_invalidation(DistributionId=distribution_id,
-                                              InvalidationBatch={
-                                                  'Paths': {
-                                                      'Quantity': 1,
-                                                      'Items': [path]
-                                                  },
-                                                  'CallerReference': str(time.time())
-                                              })
+    invalidation = client.create_invalidation(DistributionId='ESAPTUQ4RL7CE',
+        InvalidationBatch={
+            'Paths': {
+                'Quantity': 1,
+                'Items': [path]
+        },
+        'CallerReference': str(time.time())
+    })
