e283932380eb3b584881b0b3cf0f48ea332c0e53
RetroHazard <Griever92@gmail.com>
2024-05-22 11:31:50 +0900

    Update GitHub Actions to prevent unnecessary/redundant build actions during Pull Requests. Will Implement specific test actions to take place during Pull Requests instead.
    Added logic to only display Notice Banner in Development mode.

--- src/components/noticebanner.jsx
+++ src/components/noticebanner.jsx
@@ -3,25 +3,25 @@ import React from 'react';
 import {Icon} from "@iconify-icon/react";
 
 function NoticeBanner() {
-    return (
-    <>
-        <div className="inline-flex flex-row justify-center w-full">
-            <div className="flex justify-center justify-items-center bg-secondary-600 h-20 mt-5 mb-5 w-3/5">
-                <div className="icon-box mr-20">
-                    <Icon icon="fa6-solid:screwdriver-wrench" height="2em"/>
+    if (process.env.REACT_APP_DATA_SET === 'development') {
+        return (
+        <div className="inline-flex flex-row justify-center w-full flex-nowrap">
+            <div className="flex justify-center bg-secondary-600 rounded-b-xl h-20 w-fit">
+                <div className="icon-box mr-14 ml-5">
+                    <Icon icon="fa6-solid:screwdriver-wrench" height="2rem"/>
                 </div>
-                <div className="icon-box mb-1 text-center">
-                    <h1 className='text-4xl'>THIS PAGE IS UNDER CONSTRUCTION</h1>
-                    <h3 className='text-2xl'>SOME FEATURES MAY BE UNAVAILABLE</h3>
+                <div className="icon-box mb-1 mt-2 text-center">
+                    <h1 className='sm:font-light md:font-semibold sm:text-base md:text-xl lg:text-2xl '>THIS PAGE IS UNDER CONSTRUCTION</h1>
+                    <h3 className='sm:font-light md:font-semibold sm:text-sm md:text-base lg:text-lg '>SOME FEATURES MAY BE UNAVAILABLE</h3>
                 </div>
-                <div className="icon-box ml-20">
-                    <Icon icon="fa6-solid:screwdriver-wrench" height="2em"/>
+                <div className="icon-box ml-14 mr-5">
+                    <Icon icon="fa6-solid:screwdriver-wrench" height="2rem"/>
                 </div>
 
             </div>
         </div>
-    </>
-    )
+        )
+    }
 }
 
 export default NoticeBanner;
