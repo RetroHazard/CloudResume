34c8677c46671bc0de8861a7942279324d6b27f1
RetroHazard <Griever92@gmail.com>
2024-05-30 08:19:23 +0900

    Update index.jsx
    Style/Code Cleanup

--- src/pages/Skills/index.jsx
+++ src/pages/Skills/index.jsx
@@ -1,25 +1,31 @@
-import React from "react";
+import React from 'react';
 
-import SkillHighlight from "../../components/skill_highlight";
-import SkillButton from "../../components/skill_button";
-import LanguageItem from "../../components/language_item";
+import SkillHighlight from '../../components/skill_highlight';
+import SkillButton from '../../components/skill_button';
+import LanguageItem from '../../components/language_item';
 
 function Skills() {
     return (
         <>
-            <div className="content-block" id="skills">
-                <h2 className="h2 font-extrabold text-content-header mb-0">SKILLS</h2>
-                <div className="flex flex-col gap-8">
-                    <div className="flex flex-col gap-3">
-                        <SkillHighlight/>
+            <div className='content-block' id='skills'>
+                <h2 className='h2 mb-0 font-extrabold text-content-header'>
+                    SKILLS
+                </h2>
+                <div className='flex flex-col gap-8'>
+                    <div className='flex flex-col gap-3'>
+                        <SkillHighlight />
                     </div>
-                    <div className="flex flex-col gap-3">
-                        <h3 className="h3 text-content-subtitle font-extrabold mb-0">Interested in:</h3>
-                        <SkillButton/>
+                    <div className='flex flex-col gap-3'>
+                        <h3 className='h3 mb-0 font-extrabold text-content-subtitle'>
+                            Interested in:
+                        </h3>
+                        <SkillButton />
                     </div>
-                    <div className="flex flex-col gap-3">
-                        <h3 className="h3 text-content-subtitle font-extrabold mb-0">I speak</h3>
-                        <LanguageItem/>
+                    <div className='flex flex-col gap-3'>
+                        <h3 className='h3 mb-0 font-extrabold text-content-subtitle'>
+                            I speak
+                        </h3>
+                        <LanguageItem />
                     </div>
                 </div>
             </div>
