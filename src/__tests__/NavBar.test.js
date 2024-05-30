ea25318b2897fc6146714c4e1e557253d7850edc
RetroHazard <Griever92@gmail.com>
2024-05-30 14:05:19 +0900

    Unit Test Updates

--- src/__tests__/NavBar.test.js
+++ src/__tests__/NavBar.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, fireEvent } from '@testing-library/react';
-import { MemoryRouter } from 'react-router-dom';
+import { MemoryRouter, Route, Routes } from 'react-router-dom';
 
 import Navigation from '../components/navbar';
 
@@ -8,9 +8,11 @@ describe('Navigation component', () => {
     it('renders navigation links correctly', () => {
         // Render the Navigation component wrapped in MemoryRouter
         const { getByText } = render(
-            <MemoryRouter>
-                <Navigation />
-            </MemoryRouter>
+            <MemoryRouter initialEntries={['/']}>
+                <Routes>
+                    <Route path='*' element={<Navigation />} />
+                </Routes>
+            </MemoryRouter>,
         );
 
         // Assert that navigation links are rendered correctly
@@ -21,28 +23,5 @@ describe('Navigation component', () => {
         expect(getByText('Projects')).toBeInTheDocument();
         expect(getByText('Skills')).toBeInTheDocument();
         expect(getByText('Contact')).toBeInTheDocument();
-    });
-
-    it('changes style on hover', () => {
-        // Render the Navigation component wrapped in MemoryRouter
-        const { getByText } = render(
-            <MemoryRouter>
-                <Navigation />
-            </MemoryRouter>
-        );
-
-        const homeLink = getByText('Home');
-
-        // Simulate hover event
-        fireEvent.mouseEnter(homeLink);
-
-        // Assert that the style changes on hover
-        expect(homeLink).toHaveClass('nav-block-active');
-
-        // Simulate mouse leave event
-        fireEvent.mouseLeave(homeLink);
-
-        // Assert that the style reverts back after hover
-        expect(homeLink).toHaveClass('nav-block-active'); // First link should remain active even after mouse leave
     });
 });
