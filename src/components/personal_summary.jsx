6dec3e310ff197f13ade96e9ffbec4f3f5a8fd54
RetroHazard <Griever92@gmail.com>
2024-06-03 12:18:10 +0900

    Workflows Update

--- src/components/personal_summary.jsx
+++ src/components/personal_summary.jsx
@@ -6,7 +6,7 @@ function PersonalSummary() {
             <>
                 <p className='mb-0 leading-relaxed max-sm:text-xs sm:text-sm'>
                     Lorem ipsum dolor sit amet, consectetur <strong>adipiscing elit</strong>. In sodales ac dui at{' '}
-                    <em>vestibulum</em>. In condimentum metus id dui tincidunt, in blandit mi <a href='.'>vehicula</a>.
+                    <em>vestibulum</em>. In condimentum metus id dui tincidunt, in blandit mi <a href='#'>vehicula</a>.
                     Nulla lacinia, erat sit amet elementum vulputate, lectus mauris volutpat mi, vitae accumsan metus
                     elit ut nunc. Vestibulum lacinia enim eget eros fermentum scelerisque. Proin augue leo, posuere ut
                     imperdiet vitae, fermentum eu ipsum. Sed sed neque sagittis, posuere urna nec, commodo leo.
@@ -14,11 +14,11 @@ function PersonalSummary() {
                 </p>
                 <p className='mb-0 leading-relaxed max-sm:text-xs sm:text-sm'>
                     Lorem ipsum dolor sit amet, consectetur <strong>adipiscing elit</strong>. In sodales ac dui at{' '}
-                    <em>vestibulum</em>. In condimentum metus id dui tincidunt, in blandit mi{' '}
-                    <a href='/public'>vehicula</a>. Nulla lacinia, erat sit amet elementum vulputate, lectus mauris
-                    volutpat mi, vitae accumsan metus elit ut nunc. Vestibulum lacinia enim eget eros fermentum
-                    scelerisque. Proin augue leo, posuere ut imperdiet vitae, fermentum eu ipsum. Sed sed neque
-                    sagittis, posuere urna nec, commodo leo. Pellentesque posuere justo vitae massa volutpat maximus.
+                    <em>vestibulum</em>. In condimentum metus id dui tincidunt, in blandit mi <a href='#'>vehicula</a>.
+                    Nulla lacinia, erat sit amet elementum vulputate, lectus mauris volutpat mi, vitae accumsan metus
+                    elit ut nunc. Vestibulum lacinia enim eget eros fermentum scelerisque. Proin augue leo, posuere ut
+                    imperdiet vitae, fermentum eu ipsum. Sed sed neque sagittis, posuere urna nec, commodo leo.
+                    Pellentesque posuere justo vitae massa volutpat maximus.
                 </p>
             </>
         );
@@ -36,18 +36,18 @@ function PersonalSummary() {
                 <p className='mb-0 leading-relaxed max-sm:text-xs sm:text-sm'>
                     I’m driven by a passion for learning and solving complex problems. My broad industry knowledge
                     provides unique insights across multiple disciplines, enabling me to design and build secure,
-                    effective, and innovative technology solutions. I thrive on big challenges that require
+                    effective, and innovative technology solutions. I thrive when presented with challenges that require
                     out-of-the-box thinking and enjoy finding more efficient ways to achieve goals. Keeping mentally
                     engaged and continuously learning is crucial for me.
                 </p>
 
                 <p className='mb-0 leading-relaxed max-sm:text-xs sm:text-sm'>
                     Outside of work, I immerse myself in hobbies like photography, mountain biking, and music—I’ve
-                    played piano for over a <em>decade</em> and guitar for <em>five years</em>. I’m also interested in
-                    exploring woodworking and have a passion for video games, which led me to learn C++, Unreal Engine,
-                    Blender, and other 3D/VFX software(s). I’ve developed a component on the Unreal Marketplace and
-                    continue to experiment with the goal of releasing a complete commercial game. To practice and
-                    develop my skills, I run a homelab for various projects and experiments.
+                    played piano for over a <em>decade</em> and guitar for <em>five years</em>. I also have a passion
+                    for video games, which led me to learn C++, Unreal Engine, Blender, and other 3D/VFX software(s).
+                    I’ve independently developed a component on the Unreal Marketplace and continue to experiment with
+                    the goal of releasing a complete commercial game. To practice and develop my skills, I run a homelab
+                    for various projects and experiments.
                 </p>
             </>
         );
