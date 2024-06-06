5dc47ec7b306db75daade33e8afdbfe579b158fc
RetroHazard <Griever92@gmail.com>
2024-06-06 15:50:22 +0900

    KMS Successfully Deployed

--- generated/aws/kms/resources.tf
+++ generated/aws/kms/resources.tf
@@ -11,7 +11,6 @@ resource "aws_kms_key" "tfer--0fca1ad8-81ef-4b82-9662-205c52643235" {
   key_usage                = "SIGN_VERIFY"
   multi_region             = "false"
   policy                   = "{\"Id\":\"key-consolepolicy-3\",\"Statement\":[{\"Action\":\"kms:*\",\"Effect\":\"Allow\",\"Principal\":{\"AWS\":\"arn:aws:iam::339712851438:root\"},\"Resource\":\"*\",\"Sid\":\"Enable IAM User Permissions\"},{\"Action\":[\"kms:Create*\",\"kms:Describe*\",\"kms:Enable*\",\"kms:List*\",\"kms:Put*\",\"kms:Update*\",\"kms:Revoke*\",\"kms:Disable*\",\"kms:Get*\",\"kms:Delete*\",\"kms:TagResource\",\"kms:UntagResource\",\"kms:ScheduleKeyDeletion\",\"kms:CancelKeyDeletion\"],\"Effect\":\"Allow\",\"Principal\":{\"AWS\":\"AROAU6GDWXXXGJELQFKUD\"},\"Resource\":\"*\",\"Sid\":\"Allow access for Key Administrators\"},{\"Action\":[\"kms:DescribeKey\",\"kms:GetPublicKey\",\"kms:Sign\"],\"Effect\":\"Allow\",\"Principal\":{\"Service\":\"dnssec-route53.amazonaws.com\"},\"Resource\":\"*\",\"Sid\":\"Allow Route 53 DNSSEC Service\"},{\"Action\":\"kms:CreateGrant\",\"Condition\":{\"Bool\":{\"kms:GrantIsForAWSResource\":\"true\"}},\"Effect\":\"Allow\",\"Principal\":{\"Service\":\"dnssec-route53.amazonaws.com\"},\"Resource\":\"*\",\"Sid\":\"Allow Route 53 DNSSEC to CreateGrant\"}],\"Version\":\"2012-10-17\"}"
-  rotation_period_in_days  = "0"
 
   tags = {
     Name    = "DNSSEC"
