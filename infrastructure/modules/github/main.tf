1ab80a65b05a32d1013276a25b51a8fe6d77a95a
RetroHazard <griever92@gmail.com>
2024-06-15 17:53:06 +0900

    Hard Code Repo Name for GitHub module

--- infrastructure/modules/github/main.tf
+++ infrastructure/modules/github/main.tf
@@ -1,31 +1,29 @@
-data "github_user" "current" {
-  username = "RetroHazard"
-}
-
-data "github_repository" "current" {
-  full_name = "RetroHazard/CloudResume"
+variable "github_repository" {
+  description = "GitHub Repository to store Secrets and Variables in"
+  type = string
+  default = "RetroHazard/CloudResume"
 }
 
 resource "github_actions_secret" "aws-s3-key" {
-  repository  = data.github_repository.current.full_name
+  repository  = var.github_repository
   secret_name = "AWS_S3_ACCESS_KEY"
   plaintext_value = var.crc-iam-github-access-key
 }
 
 resource "github_actions_secret" "aws-s3-secret" {
-  repository  = data.github_repository.current.full_name
+  repository  = var.github_repository
   secret_name = "AWS_S3_SECRET_ACCESS_KEY"
   plaintext_value = var.crc-iam-github-secret-access-key
 }
 
 resource "github_actions_variable" "aws-s3-bucket-prod" {
-  repository    = data.github_repository.current.full_name
+  repository    = var.github_repository
   variable_name = "AWS_S3_BUCKET_PROD"
   value         = var.crc-s3-bucket-prod
 }
 
 resource "github_actions_variable" "aws-s3-bucket-stage" {
-  repository    = data.github_repository.current.full_name
+  repository    = var.github_repository
   variable_name = "AWS_S3_BUCKET_STAGE"
   value         = var.crc-s3-bucket-stage
 }
