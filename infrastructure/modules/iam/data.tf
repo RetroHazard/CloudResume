c1dc192caea10d09efc0d85b45382bd2262efc0d
RetroHazard <Griever92@gmail.com>
2024-06-17 01:05:37 +0900

    Remove Public ACL argument Revert GitHub Permissions

--- infrastructure/modules/iam/data.tf
+++ infrastructure/modules/iam/data.tf
@@ -118,13 +118,7 @@ data "aws_iam_policy_document" "crc-s3-github-actions" {
     sid     = "VisualEditor1"
     effect  = "Allow"
     actions = [
-      "s3:PutObject",
-      "s3:GetObject",
-      "s3:ListBucketMultipartUploads",
-      "s3:ListMultipartUploadParts",
-      "s3:AbortMultipartUpload",
-      "s3:DeleteObject",
-      "s3:ListBucket"
+      "s3:PutObject"
     ]
     resources = [
       var.crc-s3-production-arn,
