79f567fa26a6a9453b3d43bd729feaf5f99576fc
RetroHazard <Griever92@gmail.com>
2024-06-17 10:06:03 +0900

    Update CloudFront Distributions for OAC

--- infrastructure/modules/frontend/main.tf
+++ infrastructure/modules/frontend/main.tf
@@ -264,14 +264,7 @@ resource "aws_cloudfront_distribution" "crc-cf-production-distribution" {
   origin {
     connection_attempts = "3"
     connection_timeout  = "10"
-
-    custom_origin_config {
-      http_port                = "80"
-      https_port               = "443"
-      origin_protocol_policy   = "http-only"
-      origin_ssl_protocols     = ["SSLv3", "TLSv1", "TLSv1.1", "TLSv1.2"]
-    }
-
+    
     domain_name = aws_s3_bucket.crc-agb-s3-website-prod.bucket_regional_domain_name
     origin_id   = aws_s3_bucket.crc-agb-s3-website-prod.id
     origin_access_control_id = aws_cloudfront_origin_access_control.crc-cf-production-oac.id
@@ -343,14 +336,7 @@ resource "aws_cloudfront_distribution" "crc-cf-staging-distribution" {
   origin {
     connection_attempts = "3"
     connection_timeout  = "10"
-
-    custom_origin_config {
-      http_port                = "80"
-      https_port               = "443"
-      origin_protocol_policy   = "http-only"
-      origin_ssl_protocols     = ["SSLv3", "TLSv1", "TLSv1.1", "TLSv1.2"]
-    }
-
+    
     domain_name = aws_s3_bucket.crc-agb-s3-website-staging.bucket_regional_domain_name
     origin_id   = aws_s3_bucket.crc-agb-s3-website-staging.id
     origin_access_control_id = aws_cloudfront_origin_access_control.crc-cf-staging-oac.id
