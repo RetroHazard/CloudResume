2c065c13cbb4cf23ab9281fb3bd730c70f19d2bf
RetroHazard <Griever92@gmail.com>
2024-09-10 14:10:02 +0900

    Remove Duplicate Resource As a result of the domain migration, the A record for the www subdomain is no longer necessary.

--- infrastructure/modules/frontend/main.tf
+++ infrastructure/modules/frontend/main.tf
@@ -575,18 +575,6 @@ resource "aws_route53_record" "crc-dns-zone-record-A" {
   zone_id = aws_route53_zone.crc-new-hosted-zone.zone_id
 }
 
-resource "aws_route53_record" "crc-dns-zone-www-record-A" {
-  alias {
-    evaluate_target_health = "false"
-    name                   = aws_cloudfront_distribution.crc-cf-production-distribution.domain_name
-    zone_id                = aws_cloudfront_distribution.crc-cf-production-distribution.hosted_zone_id
-  }
-
-  name    = var.domain-name
-  type    = "A"
-  zone_id = aws_route53_zone.crc-new-hosted-zone.zone_id
-}
-
 resource "aws_route53_record" "crc-dns-zone-staging-record-A" {
   alias {
     evaluate_target_health = "false"
