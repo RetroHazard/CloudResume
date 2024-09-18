bcb4bbd850e914ea0dcf554fd814b1fdb5a7801f
Alexander Bracken <Griever92@gmail.com>
2024-09-18 11:11:51 +0900

    Domain migration: Round 3 (#59)
    * Update domain entries to Prepare for migration
    * Update Resume File with new domain
    * Update Workflows
    * Move S3 Policy Documents into frontend/data.tf
    * Add create_before_destroy to Route53 Zone Lifecycle
    * Add new resource for replacement hosted zone, Update references
    * Fix name argument in old hosted zone resource to prevent replacement
    * Attach lifecycle to new zone resource
    * Implement a more robust status check for the website workflows.
    * Remove Duplicate Resource
    * Remove Whitespace
    * Changes to Website Data
    * Fixups

--- infrastructure/modules/frontend/main.tf
+++ infrastructure/modules/frontend/main.tf
@@ -320,7 +320,7 @@ resource "aws_cloudfront_distribution" "crc-cf-staging-distribution" {
   staging          = "false"
 
   viewer_certificate {
-    acm_certificate_arn            = aws_acm_certificate.crc-sub-certificate.arn
+    acm_certificate_arn            = aws_acm_certificate.crc-website-certificate.arn
     cloudfront_default_certificate = "false"
     minimum_protocol_version       = "TLSv1.2_2021"
     ssl_support_method             = "sni-only"
@@ -351,12 +351,13 @@ resource "aws_cloudfront_function" "crc-StagingAuthorization" {
 # Begin Certificates Block #
 
 resource "aws_acm_certificate" "crc-website-certificate" {
-  domain_name       = aws_route53_zone.crc-new-hosted-zone.name
-  key_algorithm     = "RSA_2048"
-  validation_method = "DNS"
+  domain_name               = var.domain-name
+  subject_alternative_names = ["*.${var.domain-name}"]
+  key_algorithm             = "RSA_2048"
+  validation_method         = "DNS"
 
   tags = {
-    Name : var.domain-name
+    Name = var.domain-name
   }
 
   options {
@@ -370,29 +371,6 @@ resource "aws_acm_certificate" "crc-website-certificate" {
 
 resource "aws_acm_certificate_validation" "crc-website-certificate-validation" {
   certificate_arn         = aws_acm_certificate.crc-website-certificate.arn
-  validation_record_fqdns = [for record in aws_route53_record.crc-new-hosted-zone-validation-record : record.fqdn]
-}
-
-resource "aws_acm_certificate" "crc-sub-certificate" {
-  domain_name       = "*.${aws_route53_zone.crc-new-hosted-zone.name}"
-  key_algorithm     = "RSA_2048"
-  validation_method = "DNS"
-
-  tags = {
-    Name : var.domain-name
-  }
-
-  options {
-    certificate_transparency_logging_preference = "ENABLED"
-  }
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
-resource "aws_acm_certificate_validation" "crc-sub-certificate-validation" {
-  certificate_arn         = aws_acm_certificate.crc-sub-certificate.arn
   validation_record_fqdns = [for record in aws_route53_record.crc-new-hosted-zone-validation-record : record.fqdn]
 }
 
@@ -479,11 +457,6 @@ resource "aws_route53_zone" "crc-hosted-zone" {
   comment       = "Hosted Zone for Cloud Resume Project"
   force_destroy = false
   name          = "cloudresume-agb.jp"
-
-  lifecycle {
-    prevent_destroy       = true
-    create_before_destroy = true
-  }
 }
 
 resource "aws_route53_zone" "crc-new-hosted-zone" {
@@ -534,11 +507,12 @@ resource "aws_route53_record" "crc-new-hosted-zone-validation-record" {
     }
   }
 
-  name    = each.value.name
-  records = [each.value.record]
-  ttl     = 60
-  type    = each.value.type
-  zone_id = aws_route53_zone.crc-new-hosted-zone.zone_id
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = aws_route53_zone.crc-new-hosted-zone.zone_id
 }
 
 resource "aws_route53_record" "crc-dns-zone-api-record-A" {
@@ -669,7 +643,7 @@ resource "aws_api_gateway_rest_api" "crc-rest-api" {
 resource "aws_api_gateway_domain_name" "crc-api-domain" {
   depends_on      = [aws_acm_certificate_validation.crc-website-certificate-validation]
   domain_name     = "api.${var.domain-name}"
-  certificate_arn = aws_acm_certificate.crc-sub-certificate.arn
+  certificate_arn = aws_acm_certificate.crc-website-certificate.arn
   endpoint_configuration {
     types = ["EDGE"]
   }
