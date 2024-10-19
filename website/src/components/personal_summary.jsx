0666221b3a20d76b482872259a21cd66dd947d71
RetroHazard <griever92@gmail.com>
2024-10-19 19:23:47 +0900

    Update Website Minor updates to Self-summary Add AWS-SCS Certification to certification_data.json

--- website/src/components/personal_summary.jsx
+++ website/src/components/personal_summary.jsx
@@ -26,29 +26,25 @@ function PersonalSummary() {
         return (
             <>
                 <p className='mb-0 leading-relaxed max-sm:text-xs sm:text-sm'>
-                    With over <strong>15 years</strong> in the tech industry, I’ve gained a wealth of experience across
-                    roles such as client support, systems administration, systems architecture, cybersecurity, project
-                    management, and consulting. Now, I’m <em>seeking opportunities</em> in consulting, project
-                    management, or engineering, particularly in developing client solutions, leveraging{' '}
-                    <em>Cloud Technologies</em>.
+                    Drawing on <strong>15 years of experience</strong> in the tech industry, I'm a versatile
+                    professional specializing in both on-prem and cloud technologies, cybersecurity, and building
+                    innovative client solutions. My expertise spans client support, systems administration,
+                    architecture, project management, and consulting.
                 </p>
 
                 <p className='mb-0 leading-relaxed max-sm:text-xs sm:text-sm'>
-                    I’m driven by a passion for learning and solving <em>complex problems</em>. My broad industry
-                    knowledge provides <em>unique insights</em> across <strong>multiple disciplines</strong>, enabling
-                    me to design and build <em>secure</em>, <em>effective</em>, and <em>innovative</em> technology
-                    solutions. I thrive when presented with challenges that require out-of-the-box thinking and enjoy
-                    finding more <strong>efficient</strong> ways to achieve goals. Keeping mentally engaged and
-                    continuously learning is crucial for me.
+                    I’m a self-starter, driven by a passion for learning and solving <em>complex problems</em>. My
+                    diverse background allows me to approach challenges with a <em>unique perspective</em>, designing
+                    secure and efficient technology solutions that drive business growth. I thrive when presented with
+                    challenges that require out-of-the-box thinking and enjoy finding more <strong>efficient</strong>{' '}
+                    ways to achieve goals. Keeping mentally engaged and continuously learning is crucial for me.
                 </p>
 
                 <p className='mb-0 leading-relaxed max-sm:text-xs sm:text-sm'>
-                    Outside of work, whenever possible, I immerse myself in hobbies like photography, mountain biking, and music—I’ve
-                    played piano for over a <em>decade</em> and guitar for <em>five years</em>. I also have a passion
-                    for video games, which led me to learn <strong>C++</strong>, Unreal Engine, Blender, and other
-                    3D/VFX software(s). I’ve <em>independently developed</em> a component on the Unreal Marketplace and
-                    continue to experiment, with the ultimate goal of releasing a complete commercial game. To
-                    continually practice and develop my skills, I run a homelab for various projects and experiments.
+                    I constantly seek new <em>challenges and opportunities</em> to expand my skill set. My commitment to
+                    learning extends beyond the workplace, where I have spent the past few years experimenting with
+                    multiple disciplines in Game Development, from coding in <strong>C++</strong>, to 3D modeling and
+                    simulation.
                 </p>
             </>
         );
