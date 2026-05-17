33fdd9e1d089f2262e7ad48815c94ccae3f86d4c
RetroHazard <Griever92@gmail.com>
2026-05-17 11:25:04 +0900

    test: add Cypress resume download link tests

---
+++ website/cypress/e2e/resume-download.cy.js
@@ -0,0 +1,30 @@
+describe('Resume download', () => {
+  beforeEach(() => {
+    cy.stubVisitorApi();
+    cy.visit('/');
+  });
+
+  it('has a Download CV link with the download attribute', () => {
+    cy.contains('a', 'Download CV')
+      .should('have.attr', 'download');
+    cy.contains('a', 'Download CV')
+      .invoke('attr', 'href')
+      .should('exist')
+      .and('not.be.empty');
+  });
+
+  it('resume download URL responds successfully', () => {
+    cy.contains('a', 'Download CV')
+      .invoke('attr', 'href')
+      .then((href) => {
+        cy.request({ url: href, method: 'HEAD', failOnStatusCode: false })
+          .then((res) => {
+            if ([200, 301, 302].includes(res.status)) {
+              expect(res.status).to.be.oneOf([200, 301, 302]);
+            } else {
+              expect(href).to.match(/^https?:\/\/.+/);
+            }
+          });
+      });
+  });
+});
